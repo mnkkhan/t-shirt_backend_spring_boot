0e41b16798cc516d04722f7a3057e3df82b1aae1
mnkkhan <mnkkhan77@gmail.com>
2023-02-12 22:13:50 +0530

    day 1 spring boot code setup

---
+++ src/main/java/com/store/tshirtapp/TShirtAppApplication.java
@@ -0,0 +1,13 @@
+package com.store.tshirtapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TShirtAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TShirtAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/store/tshirtapp/TShirtAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.store.tshirtapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TShirtAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
